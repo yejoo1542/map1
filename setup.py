a2c37b8837cb65e88e2782807aa2b9d165e278f5
yejoo1542 <hanyejoo04@pusan.ac.kr>
2024-11-12 22:04:23 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,1 @@
-from setuptools import setup, find_packages
 
-setup(
-    name='map1',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[
-        'streamlit==1.20.0',
-        'folium==0.14.0',
-        # 추가 의존성 명시
-    ],
-)
