f5698f9633be83778ecc0646eec061e9b4d4036b
yejoo1542 <hanyejoo04@pusan.ac.kr>
2024-12-05 03:22:04 +0900

    Update web.py

--- web.py
+++ web.py
@@ -249,12 +249,7 @@ elif st.session_state.current_page == '자전거 위치 정보':
         "자전거 보관소": "images/4.png",
         "종합 병원": "images/5.png"
     }
-    # 패딩 처리된 이미지 생성
-    target_size = (300, 300)  # 고정 크기 (예: 300x300)
-    processed_images = {
-        caption: add_padding(image_path, target_size) 
-        for caption, image_path in option_images.items()
-    }
+
     # 이미지 선택 위젯
     selected_option_index = image_select(
         "자전거 관련 정보를 선택하세요:",
