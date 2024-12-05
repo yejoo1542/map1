5c08e6308097cb014ddc369f9d5f8c3f871c3599
yejoo1542 <hanyejoo04@pusan.ac.kr>
2024-12-05 17:22:56 +0900

    Update web.py

--- web.py
+++ web.py
@@ -331,7 +331,7 @@ elif st.session_state.current_page == '관광지 추천':
     image_paths = ["images/title1.jpg", "images/title2.png", "images/title3.png"]
     captions = ["지도 1", "지도 2", "지도 3"]
     urls = ["https://kko.kakao.com/1_de9FgI47", "https://kko.kakao.com/qq3xXZX0XT", "https://kko.kakao.com/7alrtOKbX3"]
-    details = ["https://imgur.com/fCPFBih", "https://imgur.com/mzPHpLr", "https://imgur.com/G9TJ4pA"]
+    details = ["https://github.com/yejoo1542/map1/blob/main/images/detail1.png", "https://github.com/yejoo1542/map1/blob/main/images/detail2.png", "https://github.com/yejoo1542/map1/blob/main/images/detail3.png"]
 
     # 상태 초기화
     if "selected_image" not in st.session_state:
